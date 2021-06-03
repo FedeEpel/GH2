16d99b32e9efb94c9350028143b82e48799c654a
jose luis oemig <jloemig@gmail.com>
2021-06-02 21:44:26 -0300

    rma22

--- uno.cpp
+++ uno.cpp
@@ -10,7 +10,8 @@ int main()
     cout << "Segundo Commit";
     cout << "Commit todo juntos en capilla";
     cout << "Estoy en la rama AYEDII y modifiquegit add 22 otro mas."; 
-    cout << "hola"; 
+    
+    cout << "hola como estas 3434333    "; 
     return 0;
 }
 
